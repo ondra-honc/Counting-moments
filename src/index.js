37edb7c0474673030b6a38a19f28340e3e4111fb
Ondra Honc <ondra.honc1@gmail.com>
2026-02-10 21:22:47 +0100

    Added: aboutMe page + finished Novel page

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var input = document.getElementById("password-input");
+var count = 0;
+function checkPassword() {
+    var secretWord = input.value.toLocaleLowerCase().trim();
+    if (secretWord === "january30") {
+        window.location.href = "novel.html";
+    }
+    else {
+        count++;
+    }
+    if (count === 1) {
+        alert("Not quite... try using the day we met MMDD");
+    }
+    else if (count >= 2) {
+        alert("Hint: it starts with J and ends with 30 - no spaces!");
+    }
+}
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        checkPassword();
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Happy coding!");
+const input = document.getElementById("password-input") as HTMLInputElement;
+
+let count = 0;
+function checkPassword() {
+    const secretWord = input.value.toLocaleLowerCase().trim();
+    if (secretWord === "january30") {
+        window.location.href = "novel.html";
+    } else {
+        count++;
+    }
+
+    if (count === 1) {
+        alert("Not quite... try using the day we met MMDD");
+    } else if (count >= 2) {
+        alert("Hint: it starts with J and ends with 30 - no spaces!");
+    }
+}
+
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    checkPassword();
+  }
+});
