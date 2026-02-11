b1e4f1950a1a848119b46c902ee907971460f16e
Ondra Honc <ondra.honc1@gmail.com>
2026-02-11 17:43:17 +0100

    Completed: about me page + added some exclusive alert lines

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,16 @@ function checkPassword() {
 
     if (count === 1) {
         alert("Not quite... try using the day we met MMDD");
-    } else if (count >= 2) {
+    } else if (count === 2) {
         alert("Hint: it starts with J and ends with 30 - no spaces!");
+    } else if (count === 3) {
+        alert("That's alright, take your time, I'm patient")
+    } else if (count === 4) {
+        alert("Should I tell you what is it?")
+    } else if (count === 5) {
+        alert("I don't think so ;)") 
+    } else if (count >= 6) {
+        alert("Attempts remaining: ∞ (I'm patient)")
     }
 }
 
